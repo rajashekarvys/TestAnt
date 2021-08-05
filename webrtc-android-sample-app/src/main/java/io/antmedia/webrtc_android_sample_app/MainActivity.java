ef1b2173e7717ec89a6dceb8ce73b675f9b13ead
rajashekar <rajashekar.vys@gmail.com>
2021-08-05 20:24:25 +0530

    first commit

---
+++ webrtc-android-sample-app/src/main/java/io/antmedia/webrtc_android_sample_app/MainActivity.java
@@ -0,0 +1,477 @@
+package io.antmedia.webrtc_android_sample_app;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.webrtc.DataChannel;
+import org.webrtc.RendererCommon;
+import org.webrtc.SurfaceViewRenderer;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Map;
+
+import de.tavendo.autobahn.WebSocket;
+import io.antmedia.webrtcandroidframework.IDataChannelObserver;
+import io.antmedia.webrtcandroidframework.IWebRTCClient;
+import io.antmedia.webrtcandroidframework.IWebRTCListener;
+import io.antmedia.webrtcandroidframework.StreamInfo;
+import io.antmedia.webrtcandroidframework.WebRTCClient;
+import io.antmedia.webrtcandroidframework.apprtc.CallActivity;
+
+import static io.antmedia.webrtcandroidframework.apprtc.CallActivity.EXTRA_CAPTURETOTEXTURE_ENABLED;
+import static io.antmedia.webrtcandroidframework.apprtc.CallActivity.EXTRA_DATA_CHANNEL_ENABLED;
+import static io.antmedia.webrtcandroidframework.apprtc.CallActivity.EXTRA_VIDEO_BITRATE;
+import static io.antmedia.webrtcandroidframework.apprtc.CallActivity.EXTRA_VIDEO_FPS;
+
+public class MainActivity extends Activity implements IWebRTCListener, IDataChannelObserver {
+
+    /**
+     * Change this address with your Ant Media Server address
+     */
+    public static final String SERVER_ADDRESS = "115.243.77.16:5080";
+
+    /**
+     * Mode can Publish, Play or P2P
+     */
+    private String webRTCMode = IWebRTCClient.MODE_PUBLISH;
+
+    private boolean enableDataChannel = true;
+
+
+    public static final String SERVER_URL = "ws://" + SERVER_ADDRESS + "/WebRTCAppEE/websocket";
+    public static final String REST_URL = "http://" + SERVER_ADDRESS + "/WebRTCAppEE/rest/v2";
+
+    private WebRTCClient webRTCClient;
+
+    private Button startStreamingButton;
+    private String operationName = "";
+    private String streamId;
+
+    private SurfaceViewRenderer cameraViewRenderer;
+    private SurfaceViewRenderer pipViewRenderer;
+    private Spinner streamInfoListSpinner;
+
+
+    // variables for handling reconnection attempts after disconnected
+    final int RECONNECTION_PERIOD_MLS = 100;
+    private boolean stoppedStream = false;
+    Handler reconnectionHandler = new Handler();
+    Runnable reconnectionRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (!webRTCClient.isStreaming()) {
+                attempt2Reconnect();
+                // call the handler again in case startStreaming is not successful
+                reconnectionHandler.postDelayed(this, RECONNECTION_PERIOD_MLS);
+            }
+        }
+    };
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set window styles for fullscreen-window size. Needs to be done before
+        // adding content.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        //getWindow().getDecorView().setSystemUiVisibility(getSystemUiVisibility());
+
+        setContentView(R.layout.activity_main);
+
+        cameraViewRenderer = findViewById(R.id.camera_view_renderer);
+        pipViewRenderer = findViewById(R.id.pip_view_renderer);
+
+        startStreamingButton = findViewById(R.id.start_streaming_button);
+
+        streamInfoListSpinner = findViewById(R.id.stream_info_list);
+
+        if(!webRTCMode.equals(IWebRTCClient.MODE_PLAY)) {
+            streamInfoListSpinner.setVisibility(View.INVISIBLE);
+        }
+        else {
+
+            streamInfoListSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                boolean firstCall = true;
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    //for some reason in android onItemSelected is called automatically at first.
+                    //there are some discussions about it in stackoverflow
+                    //so we just have simple check
+                    if (firstCall) {
+                        firstCall = false;
+                        return;
+                    }
+                    webRTCClient.forceStreamQuality(Integer.parseInt((String) adapterView.getSelectedItem()));
+                    Log.i("MainActivity", "Spinner onItemSelected");
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                }
+            });
+        }
+
+        // Check for mandatory permissions.
+        for (String permission : CallActivity.MANDATORY_PERMISSIONS) {
+            if (this.checkCallingOrSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission " + permission + " is not granted", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        if (webRTCMode.equals(IWebRTCClient.MODE_PUBLISH)) {
+            startStreamingButton.setText("Start Publishing");
+            operationName = "Publishing";
+        }
+        else  if (webRTCMode.equals(IWebRTCClient.MODE_PLAY)) {
+            startStreamingButton.setText("Start Playing");
+            operationName = "Playing";
+        }
+        else if (webRTCMode.equals(IWebRTCClient.MODE_JOIN)) {
+            startStreamingButton.setText("Start P2P");
+            operationName = "P2P";
+        }
+
+        this.getIntent().putExtra(EXTRA_CAPTURETOTEXTURE_ENABLED, true);
+        this.getIntent().putExtra(EXTRA_VIDEO_FPS, 30);
+        this.getIntent().putExtra(EXTRA_VIDEO_BITRATE, 1500);
+        this.getIntent().putExtra(EXTRA_CAPTURETOTEXTURE_ENABLED, true);
+        this.getIntent().putExtra(EXTRA_DATA_CHANNEL_ENABLED, enableDataChannel);
+
+        webRTCClient = new WebRTCClient( this,this);
+
+        //webRTCClient.setOpenFrontCamera(false);
+
+        streamId = "streamTest1";
+        String tokenId = "tokenId";
+        webRTCClient.setVideoRenderers(pipViewRenderer, cameraViewRenderer);
+
+       // this.getIntent().putExtra(CallActivity.EXTRA_VIDEO_FPS, 24);
+        webRTCClient.init(SERVER_URL, streamId, webRTCMode, tokenId, this.getIntent());
+        webRTCClient.setDataChannelObserver(this);
+
+    }
+
+
+    public void startStreaming(View v) {
+
+        if (!webRTCClient.isStreaming()) {
+            ((Button) v).setText("Stop " + operationName);
+            webRTCClient.startStream();
+            if (webRTCMode == IWebRTCClient.MODE_JOIN) {
+                pipViewRenderer.setZOrderOnTop(true);
+            }
+        }
+        else {
+            ((Button)v).setText("Start " + operationName);
+            webRTCClient.stopStream();
+            webRTCClient.startStream();
+            stoppedStream = true;
+        }
+
+    }
+
+    private void attempt2Reconnect() {
+        Log.w(getClass().getSimpleName(), "Attempt2Reconnect called");
+        if (!webRTCClient.isStreaming()) {
+            webRTCClient.startStream();
+            if (webRTCMode == IWebRTCClient.MODE_JOIN) {
+                pipViewRenderer.setZOrderOnTop(true);
+            }
+        }
+    }
+
+    @Override
+    public void onPlayStarted(String streamId) {
+        Log.w(getClass().getSimpleName(), "onPlayStarted");
+        Toast.makeText(this, "Play started", Toast.LENGTH_LONG).show();
+        webRTCClient.switchVideoScaling(RendererCommon.ScalingType.SCALE_ASPECT_FIT);
+        webRTCClient.getStreamInfoList();
+    }
+
+    @Override
+    public void onPublishStarted(String streamId) {
+        Log.w(getClass().getSimpleName(), "onPublishStarted");
+        Toast.makeText(this, "Publish started", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPublishFinished(String streamId) {
+        Log.w(getClass().getSimpleName(), "onPublishFinished");
+        Toast.makeText(this, "Publish finished", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPlayFinished(String streamId) {
+        Log.w(getClass().getSimpleName(), "onPlayFinished");
+        Toast.makeText(this, "Play finished", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void noStreamExistsToPlay(String streamId) {
+        Log.w(getClass().getSimpleName(), "noStreamExistsToPlay");
+        Toast.makeText(this, "No stream exist to play", Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    @Override
+    public void streamIdInUse(String streamId) {
+        Log.w(getClass().getSimpleName(), "streamIdInUse");
+        Toast.makeText(this, "Stream id is already in use.", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onError(String description, String streamId) {
+        Toast.makeText(this, "Error: "  +description , Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        webRTCClient.stopStream();
+    }
+
+    @Override
+    public void onSignalChannelClosed(WebSocket.WebSocketConnectionObserver.WebSocketCloseNotification code, String streamId) {
+        Toast.makeText(this, "Signal channel closed with code " + code, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDisconnected(String streamId) {
+        Log.w(getClass().getSimpleName(), "disconnected");
+        Toast.makeText(this, "Disconnected", Toast.LENGTH_LONG).show();
+
+        startStreamingButton.setText("Start " + operationName);
+        // handle reconnection attempt
+        if (!stoppedStream) {
+            Toast.makeText(this, "Disconnected Attempting to reconnect", Toast.LENGTH_LONG).show();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                if (!reconnectionHandler.hasCallbacks(reconnectionRunnable)) {
+                    reconnectionHandler.postDelayed(reconnectionRunnable, RECONNECTION_PERIOD_MLS);
+                }
+            } else {
+                reconnectionHandler.postDelayed(reconnectionRunnable, RECONNECTION_PERIOD_MLS);
+            }
+        } else {
+            Toast.makeText(this, "Stopped the stream", Toast.LENGTH_LONG).show();
+            stoppedStream = false;
+        }
+    }
+
+    @Override
+    public void onIceConnected(String streamId) {
+        //it is called when connected to ice
+        startStreamingButton.setText("Stop " + operationName);
+        // remove scheduled reconnection attempts
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (reconnectionHandler.hasCallbacks(reconnectionRunnable)) {
+                reconnectionHandler.removeCallbacks(reconnectionRunnable, null);
+            }
+        } else {
+            reconnectionHandler.removeCallbacks(reconnectionRunnable, null);
+        }
+    }
+
+    @Override
+    public void onIceDisconnected(String streamId) {
+        //it's called when ice is disconnected
+    }
+
+    public void onOffVideo(View view) {
+        if (webRTCClient.isVideoOn()) {
+            webRTCClient.disableVideo();
+        }
+        else {
+            webRTCClient.enableVideo();
+        }
+    }
+
+    public void onOffAudio(View view) {
+        if (webRTCClient.isAudioOn()) {
+            webRTCClient.disableAudio();
+        }
+        else {
+            webRTCClient.enableAudio();
+        }
+    }
+
+    @Override
+    public void onTrackList(String[] tracks) {
+
+    }
+
+    @Override
+    public void onBitrateMeasurement(String streamId, int targetBitrate, int videoBitrate, int audioBitrate) {
+        Log.e(getClass().getSimpleName(), "st:"+streamId+" tb:"+targetBitrate+" vb:"+videoBitrate+" ab:"+audioBitrate);
+        if(targetBitrate < (videoBitrate+audioBitrate)) {
+            Toast.makeText(this, "low bandwidth", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onStreamInfoList(String streamId, ArrayList<StreamInfo> streamInfoList) {
+        String[] stringArray = new String[streamInfoList.size()];
+        int i = 0;
+        for (StreamInfo si : streamInfoList) {
+            stringArray[i++] = si.getHeight()+"";
+        }
+        ArrayAdapter<String> modeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, stringArray);
+        streamInfoListSpinner.setAdapter(modeAdapter);
+    }
+
+    /**
+     * This method is used in an experiment. It's not for production
+     * @param streamId
+     */
+    public void calculateAbsoluteLatency(String streamId) {
+        String url = REST_URL + "/broadcasts/" + streamId + "/rtmp-to-webrtc-stats";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            Log.i("MainActivity", "recevied response " + response);
+                            JSONObject jsonObject = new JSONObject(response);
+                            long absoluteStartTimeMs = jsonObject.getLong("absoluteTimeMs");
+                            //this is the frame id in sending the rtp packet. Actually it's rtp timestamp
+                            long frameId = jsonObject.getLong("frameId");
+                            long relativeCaptureTimeMs = jsonObject.getLong("captureTimeMs");
+                            long captureTimeMs = frameId / 90;
+                            Map<Long, Long> captureTimeMsList = WebRTCClient.getCaptureTimeMsMapList();
+
+                            long absoluteDecodeTimeMs = 0;
+                            if (captureTimeMsList.containsKey(captureTimeMs)) {
+                                absoluteDecodeTimeMs = captureTimeMsList.get(captureTimeMs);
+                            }
+
+                            long absoluteLatency = absoluteDecodeTimeMs - relativeCaptureTimeMs - absoluteStartTimeMs;
+                            Log.i("MainActivity", "recevied absolute start time: " + absoluteStartTimeMs
+                                                        + " frameId: " + frameId + " relativeLatencyMs : " + relativeCaptureTimeMs
+                                                        + " absoluteDecodeTimeMs: " + absoluteDecodeTimeMs
+                                                        + " absoluteLatency: " + absoluteLatency);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("MainActivity", "That didn't work!");
+
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+    }
+
+    @Override
+    public void onBufferedAmountChange(long previousAmount, String dataChannelLabel) {
+        Log.d(MainActivity.class.getName(), "Data channel buffered amount changed: ");
+    }
+
+    @Override
+    public void onStateChange(DataChannel.State state, String dataChannelLabel) {
+        Log.d(MainActivity.class.getName(), "Data channel state changed: ");
+    }
+
+    @Override
+    public void onMessage(DataChannel.Buffer buffer, String dataChannelLabel) {
+        ByteBuffer data = buffer.data;
+        String messageText = new String(data.array(), StandardCharsets.UTF_8);
+        Toast.makeText(this, "New Message: " + messageText, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onMessageSent(DataChannel.Buffer buffer, boolean successful) {
+        if (successful) {
+            ByteBuffer data = buffer.data;
+            final byte[] bytes = new byte[data.capacity()];
+            data.get(bytes);
+            String messageText = new String(bytes, StandardCharsets.UTF_8);
+
+            Toast.makeText(this, "Message is sent", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Could not send the text message", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void sendTextMessage(String messageToSend) {
+        final ByteBuffer buffer = ByteBuffer.wrap(messageToSend.getBytes(StandardCharsets.UTF_8));
+        DataChannel.Buffer buf = new DataChannel.Buffer(buffer, false);
+        webRTCClient.sendMessageViaDataChannel(buf);
+    }
+
+    public void showSendDataChannelMessageDialog(View view) {
+        if (webRTCClient != null && webRTCClient.isDataChannelEnabled()) {
+            // create an alert builder
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Send Message via Data Channel");
+            // set the custom layout
+            final View customLayout = getLayoutInflater().inflate(R.layout.send_message_data_channel, null);
+            builder.setView(customLayout);
+            // add a button
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // send data from the AlertDialog to the Activity
+                    EditText editText = customLayout.findViewById(R.id.message_text_input);
+                    sendTextMessage(editText.getText().toString());
+                   // sendDialogDataToActivity(editText.getText().toString());
+                }
+            });
+            builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            // create and show the alert dialog
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this, R.string.data_channel_not_available, Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ webrtc-android-sample-app/src/test/java/io/antmedia/webrtc_android_sample_app/ExampleUnitTest.java
@@ -0,0 +1,30 @@
+package io.antmedia.webrtc_android_sample_app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+
+    /**
+     * Write test scenarios and implement it.
+     * Until that time, manuel test may be used
+     */
+    public void testStopStartStream() {
+        //1. start stream and check that it's playing
+
+        //2. stop stream and check that it's stopped
+
+        //3. start stream again and check it's is started
+    }
+}
